282dedbaca921dde1758d5c50ebc747f0957a275
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-02-24 03:57:12 +0530

    find greater element between two parameter

---
+++ ternaryOperator.js
@@ -0,0 +1,11 @@
+function findGreater(a,b){
+    return a>b ? a : b
+}
+let a=5,b=3
+console.log(findGreater(a,b))
+
+// check true false
+function checkGreater(a,b){
+    return a ? a>b : b
+}
+console.log(checkGreater(12,14))
