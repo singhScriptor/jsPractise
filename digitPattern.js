15255cd200753439aed929936a0e6351b81d425a
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-03-02 17:23:33 +0530

    print pattern in multiple of 2

--- digitPattern.js
+++ digitPattern.js
@@ -1,12 +1,12 @@
 function printPattern(n){
     for(let i=1;i<=n;i++){
         let str=''
-        for(let j=1;j<=i;j++){
-            str+=j
+        for(let j=1;j<=i*2-1;j++){
+            str+=i
 
         }
         console.log(str)
     }
 }
-let n=4
+let n=5
 printPattern(n)
