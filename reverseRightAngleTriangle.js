38220ab45ccf918a13d6f94860fdd971dd91e74f
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-03-03 03:24:10 +0530

    revRightAngleTriangle using for loop

---
+++ reverseRightAngleTriangle.js
@@ -0,0 +1,14 @@
+function revRightAngleTri(n){
+    for(let i=0;i<n;i++){
+        let str=''
+        for(let j=1;j<i+1;j++){
+            str+=' '
+        }
+        for(let k=0;k<n-i;k++){
+            str+='*'
+        }
+        console.log(str)
+    }
+}
+let n=5
+revRightAngleTri(n)
