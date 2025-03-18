38dbf2e92634d98c3ba62b7d2298b519f630cb10
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-03-19 02:12:03 +0530

    Check Equality if array

--- arrayEquality.js
+++ arrayEquality.js
@@ -1,13 +1,15 @@
 function checkEquality(arr1,arr2){
-    let arr=arr1.sort((a,b)=>a-b)
-    let jrr=arr2.sort((a,b)=>a-b)
-    if(arr.length !== jrr.length )return false
+    let arr=arr1.sort()
+    let jrr=arr2.sort()
+    if(arr.length !== jrr.length){
+        return "False"
+    }
     for(let i=0;i<arr.length;i++){
         if(arr[i] !== jrr[i]){
-            return false
+            return "False"
         }
     }
-    return true
+    return "True"
 }
-let arr1=[3,7,2,9,6],arr2=[3,7,2,9,6]
+let arr1=[3,7,2,8,6],arr2=[3,7,2,8,6]
 console.log(checkEquality(arr1,arr2))
