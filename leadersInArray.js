9cb8895f2412d9a71ea2e3278ee2b89d253f9147
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-03-21 11:45:15 +0530

    leaders in array using an array

--- leadersInArray.js
+++ leadersInArray.js
@@ -1,12 +1,13 @@
 function leadersInArray(arr){
     let jrr=[],maxi=-Infinity
     for(let i=arr.length;i>=0;i--){
-        if(arr[i]>maxi){
+        if(arr[i] > maxi){
             maxi=arr[i]
-            jrr.push(maxi)
+            jrr.push(arr[i])
         }
     }
     return jrr.reverse()
+
 }
 let arr=[16,17,4,3,5,2]
 console.log(leadersInArray(arr))
