b1864463eff7c27d8f6d7254cfe0dfb51285a8b3
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-03-21 11:57:34 +0530

    missing number from an array

---
+++ missingNumber.js
@@ -0,0 +1,11 @@
+function missingNumber(arr){
+    arr.sort((a, b) => a - b);
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] !== i){
+            return i
+        }
+    }
+    return arr.length
+}
+let arr=[9,6,4,2,3,5,7,0,1]
+console.log(missingNumber(arr))
