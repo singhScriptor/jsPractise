c9037df642de65e0e2d9a3c45d70ba05aaab3250
DHIRAJ KR <dhirajkr1117@gmail.com>
2025-04-30 22:24:44 +0530

    created new folder for basics practise & added two code using if else and swapping method

---
+++ easyCodes/largestAmongTwo.js
@@ -0,0 +1,6 @@
+function findLargest(a,b){
+    if(a>b)return a
+    return b
+}
+let a=13,b=16
+console.log(findLargest(a,b))
---
+++ easyCodes/swapNumbers.js
@@ -0,0 +1,7 @@
+function swapNumbers(a,b){
+    let swap=a 
+    a=b
+    b=swap
+    return [a,b]
+}
+console.log(swapNumbers(12,14))
